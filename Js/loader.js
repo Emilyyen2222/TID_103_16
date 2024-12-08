e09e1dfabec2ab34c5d0c3259bf2315dc4c6f3fe
Emilyyen222 <yentzu2222@gmail.com>
2024-12-08 23:06:23 +0800

    loader update

---
+++ Js/loader.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    const loader = document.getElementById('loader');
+    const content = document.querySelector('.content');
+
+    // 等待動畫結束後顯示內容
+    setTimeout(() => {
+        loader.style.display = 'none'; 
+        // content.style.display = 'block'; 
+        body.style.overflow='hidden';
+    }, 2500); 
+};
